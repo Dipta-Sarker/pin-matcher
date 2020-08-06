e17eacfc25d4ba5291e0f164d399ab48a86605c9
Dipta-Sarker <contact.diptasarker@gmail.com>
2020-08-06 15:59:36 +0600

    website complete

---
+++ script.js
@@ -0,0 +1,55 @@
+// Number capture area
+const number = document.getElementsByClassName("number");
+for (let i = 0; i < number.length; i++) {
+    const element = number[i];
+    element.addEventListener("click", function(){ 
+        let screen = document.getElementById("matching-pin").value;
+        screen = screen + this.id;
+        document.getElementById("matching-pin").value = screen;
+    })
+}
+
+// Clear and backspace area
+const operatorBtn = document.getElementsByClassName ("button");
+for (let i = 0; i < operatorBtn.length; i++) {
+    const operator = operatorBtn[i];
+    operator.addEventListener("click", function(){
+        if(this.id == "clear"){
+            document.getElementById("matching-pin").value = "";
+        }
+        else if(this.id == "backspace"){
+            let backspace = document.getElementById("matching-pin").value;
+            backspace = backspace.slice(0,backspace.length-1);
+            document.getElementById("matching-pin").value = backspace;
+        }
+
+    })
+} 
+// Generate button function
+const generateBtn = document.getElementById("generate-btn");
+generateBtn.addEventListener("click", function(){
+    const generatePin = Math.round(1000 + Math.random()*9000);
+    document.getElementById("generate-pin").value = generatePin;
+
+})
+
+//Pin matching area
+const submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener("click", function(){
+    const generateOutput = document.getElementById("generate-pin").value;
+    const screenOutput =  document.getElementById("matching-pin").value;
+    if(screenOutput == ""){
+        alert("please enter a valid code")
+    }
+    else if(generateOutput == screenOutput){
+        document.getElementById("right-pin").style.display = "block";
+        document.getElementById("wrong-pin").style.display = "none";
+    }
+    else if(generateOutput !== screenOutput){
+        document.getElementById("right-pin").style.display = "none";
+        document.getElementById("wrong-pin").style.display = "block";
+    }
+})
+
+    document.getElementById("right-pin").style.display = "none";
+    document.getElementById("wrong-pin").style.display = "none";
